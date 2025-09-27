bef5921ef8329b1a8725ca0219ec3d9032b35841
drchiller <chiller629@gmail.com>
2025-09-27 17:23:54 -0500

    stage and game code

--- main/game.gd
+++ main/game.gd
@@ -2,16 +2,14 @@ extends Node2D
 
 const HAZARD = preload("uid://d1mn45ydc6cma")
 const max_rooms = 3
-
-
-@onready var timer: Timer = $Timer
-@onready var label: Label = $Label
+# width of shop is 300 pixels
+const stage_size = Vector2(1620, 1080)
 
 @onready var stage0:PackedScene = preload("res://main/stages/stage0.tscn")
 @onready var stage1:PackedScene = preload("res://main/stages/stage0.tscn")
 @onready var stage2:PackedScene = preload("res://main/stages/stage0.tscn")
 
-@onready var stages:Array = [stage0, stage1, stage2]
+@onready var total_stages:Array = [stage0, stage1, stage2]
 
 
 # prolly don't need 
@@ -19,15 +17,14 @@ const max_rooms = 3
 	get:
 		return get_tree().get_nodes_in_group("Rooms")
 
-var traps_left := 4
+#var traps_left := 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$TrapTimer.start(5)
-
 	#autoload scenes
 	_choose_scenes()
-
+	
+	#$Player.global_position = 
 
 	# timer.timeout.connect(func():
 	# 	traps_left += 1
@@ -36,28 +33,51 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
-func _process(delta: float) -> void:
-	label.text = "Traps Left: %s" % traps_left
-	if Input.is_action_just_pressed("click") and traps_left > 0:
-		traps_left -= 1
-		var pos := get_viewport().get_mouse_position()
-		var n := HAZARD.instantiate()
-		n.position = pos
-		add_child(n)
+func _process(_delta: float) -> void:
+	$Camera.position.y = $Player.position.y
+	
+	$Camera.position.y = min($Player.position.y-500, 0)
+
+	#label.text = "Traps Left: %s" % traps_left
+	#if Input.is_action_just_pressed("click") and traps_left > 0:
+	#	traps_left -= 1
+	#	var pos := get_viewport().get_mouse_position()
+	#	var n := HAZARD.instantiate()
+	#	n.position = pos
+	#	add_child(n)
 
 func _choose_scenes() -> void:
 	# choose 3 rooms at a random order and sequence
-	stages.duplicate().shuffle().slice(0, 2)
+	# var stages = total_stages.duplicate().shuffle().slice(0, 2)
+	var stages = total_stages.duplicate()
+	stages.shuffle()
+	stages = stages.slice(0, 2)
+	
+	# need to add floor to 1st stage always - better way to do this ? 
+	var first_scene = stages.pop_at(0)
+	var first_room = first_scene.instantiate()
+	var pos = first_room.global_position
+	first_room.add_to_group("Rooms")
+	add_child(first_room)
+	#var coll = get_node("Areas/Boundaries/Bottom")
+	#var shape = RectangleShape2D.new()
+	#shape.size = Vector2(1620, 50)
+	#coll.shape = shape
+	
+	# add the remaining 
+	for stage in stages:
+		var room = stage.instantiate()
+		pos += stage_size
+		room.global_position = pos
+		room.add_to_group("Rooms")
+		add_child(room)
 
-	#for stage in stages: 
-		#stage 
+#func _on_stage_zone_body_entered(body: Node2D) -> void:
+#	if body.is_in_group("player"):
+#		print('next stage up')
 
-func _on_stage_zone_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		print('next stage up')
-
-func _on_trap_timer_timeout():
-	if traps_left < 5:
-		traps_left += 1
+#func _on_trap_timer_timeout():
+#	if traps_left < 5:
+#		traps_left += 1
 	
 	
