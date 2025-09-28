e0a4ba6933590a13fb92c6c6c59d3e6b60d402c2
Jay <91770194+jaymehta-g@users.noreply.github.com>
2025-09-28 11:24:14 -0500

    Swap state placeholder. Goal added. Game state now swaps to the swap state on goal reached

--- main/ingame_main_state.gd
+++ main/ingame_main_state.gd
@@ -1,6 +1,15 @@
 extends State
 
+@export var swap_state: State
 @export var game_scene: PackedScene
+var game_node: Node
 
 func _state_enter() -> void:
-	actor.add_child(game_scene.instantiate())
+	game_node = game_scene.instantiate()
+	actor.add_child(game_node)
+	SignalBus.goal_reached.connect(func():
+		fsm.switch_state(swap_state)
+		)
+
+func _state_exit() -> void:
+	game_node.queue_free()
