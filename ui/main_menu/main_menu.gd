de1cf705ac53ee639da330c2a75c39b811de2cc6
Jay <91770194+jaymehta-g@users.noreply.github.com>
2025-09-28 17:26:40 -0500

    menu fadeout and sound

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -2,8 +2,16 @@ extends CanvasLayer
 
 signal start
 @onready var start_btn := %"Start Button" as Button
+@export var ui_sound: AudioStreamPlayer
+@export var fadeout: AnimationPlayer
 
 func _ready() -> void:
-    start_btn.pressed.connect(func(): 
-        start.emit()
-    )
+	start_btn.pressed.connect(_on_start_button)
+
+func _on_start_button():
+	start_btn.pressed.disconnect(_on_start_button)
+	ui_sound.play()
+	fadeout.play(&"fadeout")
+	fadeout.animation_finished.connect(func(_a):
+		start.emit()
+	)
